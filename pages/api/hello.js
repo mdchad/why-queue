d781ee08f1a9d9b91a30cc64150af0173c0a9601
mdchad <irsyad.muhd@gmail.com>
2020-10-26 02:08:18 +0800

    init, call success, message success

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const accountSid = 'AC46d64dba6bf54d2d5e95b2498704ba57';
+const authToken = 'ae6e50a237ec59ec2b159b2a9471cc44';
+const client = require('twilio')(accountSid, authToken);
+
 export default (req, res) => {
+  const {
+    method,
+  } = req;
+
+  if (method === 'POST') {
+    const parsed = JSON.parse(req.body);
+    console.log(parsed)
+    // client.messages
+    //     .create({body: 'Abang munir. I love you so much. Jom tido sayang!', from: 'Sofia', to: parsed.data})
+    //     .then(message => console.log(message.sid))
+    //     .catch(e => console.log(e));
+    client.calls
+        .create({
+          twiml: '<Response><Say>Hello there General Kenbobi. We meet again!</Say></Response>',
+          to: parsed.data,
+          from: '+19252557392'
+        })
+        .then(call => console.log(call.sid))
+        .catch(e => console.log(e));
+  } else {
+    // Handle any other HTTP method
+  }
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,62 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import TextField from "@material-ui/core/TextField";
+import {useState} from "react";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 export default function Home() {
+  const [num, setNum] = useState(null)
+
+  function handleChange(e) {
+    setNum(e.target.value)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    console.log(num)
+    // const stringifyNumber = String(num)
+    try {
+      const res = await fetch("/api/hello", {
+        method: "POST",
+        body: JSON.stringify({ data: '+65' + num }),
+      });
+      setNum('')
+    } catch (e) {
+      console.error("error");
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Skip the Q</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
+      <header className={styles.header}>
+        <h1>Skip the queue. We will notify you ⏰</h1>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label}>Please enter your number to  be on the wait list.</label>
+          <TextField
+              id="outlined-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              required={true}
+              value={num}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">🇸🇬 +65</InputAdornment>,
+              }}
+          />
+        </form>
       </main>
 
       <footer className={styles.footer}>
